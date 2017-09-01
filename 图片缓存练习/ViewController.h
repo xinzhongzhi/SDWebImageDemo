ef34712d20eeb76e2d79eab2724999a8eac49192
risenb_xinzhongzhi <xinzz_sy_risenb@163.com>
2017-09-01 14:05:48 +0800

    Initial Commit

---
+++ 图片缓存练习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片缓存练习
+//
+//  Created by 辛忠志 on 2017/9/1.
+//  Copyright © 2017年 辛忠志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 图片缓存练习/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  图片缓存练习
+//
+//  Created by 辛忠志 on 2017/9/1.
+//  Copyright © 2017年 辛忠志. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
